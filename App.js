5eb927c77101d0acb0ed02e22e3ce349feb33a17
GratschW <158864518+GratschW@users.noreply.github.com>
2024-03-04 15:26:00 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ExerciseSelection from './menu';
 import RepetitionExercise from './repetition';
 import DurationExercise from './duration';
+import BenchExercise from './bench'; 
 
 const App = () => {
   const [selectedExercise, setSelectedExercise] = useState(null);
@@ -12,19 +13,19 @@ const App = () => {
 
   return (
     <div>
-      {}
       {selectedExercise === null && <ExerciseSelection onSelect={handleExerciseSelection} />}
 
-      {}
       {selectedExercise === 'repetition' && <RepetitionExercise onSelect={handleExerciseSelection} />}
 
-      {}
       {selectedExercise === 'duration' && <DurationExercise onSelect={handleExerciseSelection} />}
+
+      {selectedExercise === 'bench' && <BenchExercise onSelect={handleExerciseSelection} />} {/* Render BenchExercise when selectedExercise is 'bench' */}
     </div>
   );
 };
 
 export default App;
+
 
 
 
--- menu.jsx
+++ menu.jsx
@@ -4,12 +4,11 @@ const ExerciseSelection = ({ onSelect }) => {
   return (
     <div>
       <h2>Exercises</h2>
-      <button onClick={() => onSelect('repetition')}>Repetition</button>
-      <button onClick={() => onSelect('duration')}>Duration</button>
+      <button onClick={() => onSelect('repetition')}>Push Ups</button>
+      <button onClick={() => onSelect('duration')}>Running</button>
+      <button onClick={() => onSelect('bench')}>Bench Press</button>
     </div>
   );
 };
 
 export default ExerciseSelection;
-
-  
